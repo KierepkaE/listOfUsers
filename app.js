6e01a719775d5eb8fa1330884528199622a29534
kierepkae <kierepkaedyta@gmail.com>
2018-12-18 18:41:07 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const data = {
+  users: [
+    {
+      id: 1,
+      age: 29,
+      name: "Nate"
+    },
+    {
+      id: 2,
+      age: 49,
+      name: "Adam"
+    },
+    {
+      id: 3,
+      age: 19,
+      name: "Jane"
+    }
+  ]
+};
+
+const Item = props => {
+  return (
+    <div>
+      <h1>User: {props.content.name}</h1>
+      <h2>Age: {props.content.age}</h2>
+    </div>
+  );
+};
+
+class ListItems extends React.Component {
+  render() {
+    const users = this.props.data.users;
+    const Items = users.map(user => <Item key={user.id} content={user} />);
+
+    return (
+      <>
+        <ul>{Items}</ul>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<ListItems data={data} />, document.getElementById("root"));
